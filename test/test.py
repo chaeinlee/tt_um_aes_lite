e178b8a7bebc9a9d2b141c71c97b54c318a68d51
chaeinlee <misvtsaranghae@gmail.com>
2025-10-28 20:43:16 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_loopback(dut):
+async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -17,56 +17,24 @@ async def test_loopback(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-
-    # ui_in[0] == 0: Output is uio_in
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    for i in range(256):
-        dut.uio_in.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
+    dut._log.info("Test project behavior")
 
-    # When under reset: Output is uio_in, uio is in input mode
-    dut.rst_n.value = 0
+    # Set the input values you want to test
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
+
+    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-    assert dut.uio_oe.value == 0
-    for i in range(256):
-        dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
 
-@cocotb.test()
-async def test_counter(dut):
-    dut._log.info("Start")
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 50
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # ui_in[0] == 1: bidirectional outputs enabled, put a counter on both output and bidirectional pins
-    dut.ui_in.value = 1
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 2)
-
-    dut._log.info("Testing counter")
-    for i in range(256):
-        assert dut.uo_out.value == dut.uio_out.value
-        assert dut.uo_out.value == i
-        await ClockCycles(dut.clk, 1)
-
-    dut._log.info("Testing reset")
-    for i in range(5):
-        assert dut.uo_out.value == i
-        await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 2)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.v
